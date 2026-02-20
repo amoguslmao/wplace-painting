2b3632820aa17d574f585ee21788809909b34f1c
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2026-02-20 20:49:12 +0700

    feat(api): add endpoint to delete account

---
+++ src/controllers/accounts/deleteAccount.ts
@@ -0,0 +1,33 @@
+import type { Request, Response } from "express";
+import { AccountManager } from "../../services/accountManager.js";
+
+export default function deleteAccount(req: Request<{ id: string }, {}, {}>, res: Response) {
+	const accountId = Number(req.params.id);
+
+	if (!accountId) {
+		return res.status(400).json({
+			message: "Account ID doesnt valid."
+		});
+	}
+
+	const accountManager = AccountManager.getInstance();
+
+	if (!accountManager.accounts.has(accountId)) {
+		return res.status(400).json({
+			message: `Doesnt have any account with id ${accountId}`
+		});
+	}
+
+	try {
+		accountManager.deleteAccount(accountId);
+
+		res.status(200).json({
+			message: `Successfully`
+		});
+	} catch (err) {
+		res.status(500).json({
+			message: "Server got an error while deleting account",
+			cause: err
+		});
+	}
+}
--- src/routers/accounts/index.ts
+++ src/routers/accounts/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import getAllAccounts from "../../controllers/accounts/getAllAccounts.js";
 import addNewAccount from "../../controllers/accounts/addNewAccount.js";
+import deleteAccount from "../../controllers/accounts/deleteAccount.js";
 
 const router = Router();
 
 router.get(`/`, getAllAccounts);
 router.post(`/add`, addNewAccount);
+router.delete(`/:id`, deleteAccount)
 
 export default router;
