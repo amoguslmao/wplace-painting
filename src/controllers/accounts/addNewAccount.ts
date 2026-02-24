4a640d9ce92edda11fa838bf98744c516c051ef6
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2026-02-24 23:15:44 +0700

    feat(api): use SSE to stream data when adding multi tokens

--- src/controllers/accounts/addNewAccount.ts
+++ src/controllers/accounts/addNewAccount.ts
@@ -1,29 +1,61 @@
 import type { Request, Response } from "express";
 import type { NeedJwtToken } from "../../types/request.js";
 import { AccountManager } from "../../services/accountManager.js";
+import { sleep } from "../../utils/promises.js";
 
 export default async function addNewAccount(req: Request<{}, {}, NeedJwtToken>, res: Response) {
-	if (!req.body.jwtToken) {
+	if (!req.body.tokens) {
 		return res.status(400).json({
-			message: "Field 'jwtToken' is required"
+			message: "Field 'jwtTokens' is required"
+		});
+	}
+
+	if (!Array.isArray(req.body.tokens)) {
+		return res.status(400).json({
+			message: `Field "jwtTokens" must be an array`
 		});
 	}
 	
-	const { jwtToken } = req.body;
+	const { tokens } = req.body;
 
 	try {
 		const accountManager = AccountManager.getInstance();
 
-		const result = await accountManager.addAccount(jwtToken);
+		res.setHeader("Content-Type", "text/event-stream");
+		res.setHeader("Cache-Control", "no-cache");
+		res.setHeader("Connection", "keep-alive");
 
-		res.status(200).json({
-			message: "Successfully",
-			addition: result
-		});
+		for (let i = 0; i < tokens.length; i++) {
+			if (i + 1 <= tokens.length && i > 0) {
+				// TODO: change the behavior when fetching each account
+				await sleep(5000);
+			}
+
+			const result = await accountManager.addAccount(tokens[i]);
+
+			res.write(
+				sendSSE(JSON.stringify({ message: result.message }), result.status)
+			);
+		}
+
+		res.end();
 	} catch (error) {
-		res.status(502).json({
-			message: "Could not add account",
-			cause: error
-		});
+		res.write(
+			sendSSE(JSON.stringify({
+				message: "An error ocurred when fetching",
+				cause: error
+			}), "error")
+		);
+
+		res.end();
+
+		console.error(`An error occurred in endpoint ${req.url}`, error);
 	}
+}
+
+function sendSSE(data: string, event: "success" | "failed" | "error"): string {
+	const eventLine = `event: ${event}`;
+	const dataLine = `data: ${data}`;
+
+	return `${eventLine}\n${dataLine}\n\n`;
 }
