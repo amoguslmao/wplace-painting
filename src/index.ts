1dd6234f329b075ee560409ad3924a4a6a916c3b
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2026-02-24 23:04:53 +0700

    fix(api): update the limit of the json body

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ console.log(`Account Manager is initilized`);
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.static("./public"));
 app.use("/api", routers);
 
