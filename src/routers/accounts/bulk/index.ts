1087a070e10672ab282c4516ec9349b35c046666
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2026-03-07 18:24:42 +0700

    feat(api): add router for bulk action

--- src/routers/accounts/bulk/index.ts
+++ src/routers/accounts/bulk/index.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+import bulkFetchUser from "../../../controllers/accounts/bulk/fetchUser.js";
+import bulkJoinAlliance from "../../../controllers/accounts/bulk/joinAlliance.js";
+import bulkLeaveAlliance from "../../../controllers/accounts/bulk/leaveAlliance.js";
+
 const router = Router();
+
+router.post(`/user/fetch`, bulkFetchUser);
+router.post(`/user/alliance/join`, bulkJoinAlliance);
+router.delete(`/user/alliance/leave`, bulkLeaveAlliance);
 
 export default router;
