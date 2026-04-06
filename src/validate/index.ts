b6d7ae4869029ca39e5f7bca381e66a590de613f
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-04-06 18:40:36 +0700

    feat(validate): use zod to validate the schema

---
+++ src/validate/index.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+export const UsedColorSchema = z.object({
+	total: z.number().int().nonnegative(),
+	colorId: z.number().int().nonnegative(),
+});
+
+export const TemplateImageInformationSchema = z.object({
+	width: z.number().int().positive(),
+	height: z.number().int().positive(),
+	usedColors: z.array(UsedColorSchema),
+});
+
+export const TemplateCoordinatesSchema = z.tuple([
+	z.number().int(),      // tileX
+	z.number().int(),      // tileY
+	z.number().int(),      // pixelX
+	z.number().int(),      // pixelY
+]);
+
+export const TemplateSettingSchema = z.object({
+	paintTransparentPixels: z.boolean(),
+	skipPaintedPixels: z.boolean(),
+	outlineFirst: z.boolean(),
+	autoStart: z.boolean(),
+	purchase: z.object({
+		buyMaxCharges: z.union([z.literal(false), z.number().int().nonnegative()]),
+		buyPaintCharges: z.boolean(),
+		buyPremiumColors: z.boolean(),
+	}),
+});
+
+export const TemplateInformationSchema = z.object({
+	name: z.string().min(1),
+	imageName: z.string().min(1),
+
+	imageInformation: TemplateImageInformationSchema,
+	assignedAccounts: z.array(z.number().int().nonnegative()),
+	coordinates: TemplateCoordinatesSchema,
+	setting: TemplateSettingSchema,
+});
