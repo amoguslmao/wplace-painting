e22bc9b021037585ebf9f74b4652e3105f22ed61
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-04-10 21:56:50 +0700

    feat(router): rename the controller and use multer for handling file

--- src/routers/image/index.ts
+++ src/routers/image/index.ts
@@ -1,23 +1,29 @@
 import { Router } from "express";
 import multer from "multer";
 
-import uploadImage from "../../controllers/image/upload.js";
-import getImages from "../../controllers/image/get.js";
-import listImages from "../../controllers/image/list.js";
+import uploadImage from "../../controllers/image/uploadImage.js";
+import getImageByName from "../../controllers/image/getImageByName.js";
+import imagePalleteCheck from "../../controllers/image/palleteCheck.js";
 
 const router = Router();
 
-const ml = multer({ 
+const diskStorage = multer({ 
 	storage: multer.diskStorage({
 		destination: "./database/uploads",
 		filename(_, file, callback) {
-			callback(null, file.originalname);
+			const { originalname } = file;
+
+			callback(null, originalname.trim().replaceAll(" ", "_"));
 		}
 	})
 });
 
-route.post("/upload", ml.single("image"), uploadImage);
-route.get("/list", listImages);
-route.get("/:imageName", getImages);
+const memStorage = multer({
+	storage: multer.memoryStorage(),
+})
+
+router.post("/pallete-check", memStorage.single("image"), imagePalleteCheck);
+router.post("/", diskStorage.single("image"), uploadImage);
+router.get("/:imageName", getImageByName);
 
 export default router;
