5774b25baf4a9d2089b8ea8a12365aac31968b7e
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-04-26 21:09:48 +0700

    feat(router): add endpoint to update template

--- src/routers/templates/index.ts
+++ src/routers/templates/index.ts
@@ -5,13 +5,16 @@ import getAllTemplates from "../../controllers/templates/getAllTemplates.js";
 import getTemplateById from "../../controllers/templates/getTemplateById.js";
 import deleteTemplateById from "../../controllers/templates/deleteTemplateById.js";
 import changeTemplateStatus from "../../controllers/templates/changeTemplateStatus.js";
+import updateTemplateById from "../../controllers/templates/updateTemplateById.js";
 
 const router = Router();
 
 router.get("/", getAllTemplates);
 router.post("/", addNewTemplate);
+
 router.get("/:id", getTemplateById);
 router.delete("/:id", deleteTemplateById);
+router.put("/:id", updateTemplateById);
 
 router.patch("/:id/status", changeTemplateStatus);
 
