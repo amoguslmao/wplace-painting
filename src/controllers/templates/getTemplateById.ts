920b3734e6ae26abc2bfd6dd470a490a60abca74
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-04-26 11:37:24 +0700

    feat(api): add endpoint to get template by id

---
+++ src/controllers/templates/getTemplateById.ts
@@ -0,0 +1,28 @@
+import type { Request, Response } from "express";
+import { TemplateManager } from "../../services/templateManager.js";
+
+export default async function getTemplateById(req: Request<{ id: number }>, res: Response) {
+	const templateId = Number(req.params.id);
+
+	const templateManager = TemplateManager.getInstance();
+
+	const template = templateManager.templates.get(templateId);
+
+	if (!template) {
+		return res.status(404).json({
+			message: `Could not find template with ID ${req.params.id}`
+		});
+	}
+
+	return res.status(200).json({
+		id: template.id,
+		name: template.name,
+		createdAt: template.createdAt,
+		imageName: template.imageName,
+
+		imageInformation: template.imageInformation,
+		assignedAccounts: template.assignedAccounts,
+		coordinates: template.coordinates,
+		setting: template.setting
+	});
+}
--- src/routers/templates/index.ts
+++ src/routers/templates/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import addNewTemplate from "../../controllers/templates/addTemplate.js";
 import getAllTemplates from "../../controllers/templates/getAllTemplates.js";
+import getTemplateById from "../../controllers/templates/getTemplateById.js";
 
 const router = Router();
 
 router.get("/", getAllTemplates);
 router.post("/", addNewTemplate);
+router.get("/:id", getTemplateById);
 
 export default router;
