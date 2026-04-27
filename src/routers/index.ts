60f4075feb16f0649c5c12f1ea899c86181ad313
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-04-27 18:20:48 +0700

    feat(routers): add router 'app'

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import image from "./image/index.js";
 import accounts from "./accounts/index.js";
 import templates from "./templates/index.js";
+import app from "./app/index.js";
 
 const route = Router();
 
 route.use("/image", image);
 route.use("/accounts", accounts);
 route.use("/templates", templates);
+route.use("/app", app);
 
 export default route;
