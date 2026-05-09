66f820253dba98a075bbed60d52ae47a0ae6085f
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-05-09 22:35:12 +0700

    feat(types): add 'PixelInformation' and 'PaintingTile' interface

--- src/types/pixel.d.ts
+++ src/types/pixel.d.ts
@@ -9,4 +9,20 @@ export interface Tile {
 	y: number
 }
 
+export interface PixelInformation {
+	x: number,
+	y: number,
+	color: number
+}
+
+export interface PaintingTile {
+	x: number,
+	y: number,
+	pixels: {
+		x: number[],
+		y: number[],
+		colors: number[]
+	}
+}
+
 export type GlobalPixel = Omit<Pixel, "tile">;
