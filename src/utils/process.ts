3fd5d52bcd9e4be341030b85bbcb60488fd65c40
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-05-11 23:18:00 +0700

    feat(utils): add function to send message between child and main process

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -1,3 +1,4 @@
+import type { ChildProcess, Serializable } from "node:child_process";
 import type { DatabaseInstance } from "../services/database.js";
 
 export function exit(db: DatabaseInstance) {
@@ -6,4 +7,8 @@ export function exit(db: DatabaseInstance) {
 	console.log(`Database closed`);
 
 	process.exit(0);
+}
+
+export function sendMessage<Type extends Serializable>(process: NodeJS.Process | ChildProcess, message: Type) {
+	return process.send?.(message);
 }
