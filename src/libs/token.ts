f766d90d1b530ca735c028e5a718305013b148f7
ImaoguSvn <112055678+amoguslmao@users.noreply.github.com>
2026-06-20 17:08:33 +0700

    fix(libs): remove lib jwtTokenParser and add lib token

---
+++ src/libs/token.ts
@@ -0,0 +1,19 @@
+import type { HeaderJWT, ParsedJWT, PayloadJWT } from "../types/token.js";
+
+export function parseJWT(token: string): ParsedJWT {
+	const [headerB64, payloadB64, signature] = token.split(".");
+
+	const header = JSON.parse(
+		Buffer.from(headerB64, "base64").toString(),
+	) as HeaderJWT;
+
+	const payload = JSON.parse(
+		Buffer.from(payloadB64, "base64").toString(),
+	) as PayloadJWT;
+
+	return {
+		header,
+		payload,
+		signature,
+	};
+}
